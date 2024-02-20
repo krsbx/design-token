c10a7bd32299e572908fd7d14b65b6b418d690f3
Muhammad Firdaus Sati <firdaus.sati51@gmail.com>
2024-02-21 06:52:41 +0700

    feat: main package file

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { DesignTokenTransformer } from './bin/cli';
+export { extractTokens } from './utils/extractor';
+export { generateCss } from './utils/generator';
+export { appendStyle } from './utils/styles';
+export { writeCss } from './utils/writer';
